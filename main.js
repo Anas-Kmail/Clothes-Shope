5d8afbab8b9db10b920dd1dd82fca98861520fef
Anas-Kmail <145199331+Anas-Kmail@users.noreply.github.com>
2024-08-16 20:19:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+let Menuitem=document.querySelector("nav");
+function menutoggle()
+{
+    Menuitem.classList.toggle("d-dis")
+
+}
+
+let largeimg=document.querySelector(".productimg");
+
+let smallimg=document.querySelectorAll(".smallimage");
+console.log(smallimg);
+
+for(let i=0;i<smallimg.length;i++)
+{
+smallimg[i].getAttribute("src");
+console.log(smallimg[i].src);
+smallimg[i].onclick=function()
+{
+largeimg.src=smallimg[i].src;
+
+
+}
+
+
+}
+
+let login=document.querySelector(".login-form");
+let regin=document.querySelector(".register-form");
+let indicator=document.querySelector(".indicator");
+console.log(indicator);
+
+function Login(){
+    regin.style.transform="translateX(300px)";
+    login.style.transform="translateX(300px)";
+    indicator.style.transform="translateX(0px)";
+
+
+}
+
+function regist(){
+
+regin.style.transform="translateX(0px)";
+login.style.transform="translateX(0px)";
+indicator.style.transform="translateX(100px)";
+
+}
